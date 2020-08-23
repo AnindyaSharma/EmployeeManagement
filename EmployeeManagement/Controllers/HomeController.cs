88242be0201e7b7b58be438ca4c070e17e644a7c
AnindyaSharma <anindyasharma010@gmail.com>
2020-08-23 15:24:56 +0600

    initial commit

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+
+        // Inject IEmployeeRepository using Constructor Injection
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        // Retrieve employee name and return
+        public string Index()
+        {
+            return _employeeRepository.GetEmployee(1).Name;
+        }
+    }
+}
